2b4fc14d30016b00f4343b472f25ee7ee163cf76
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2022-09-13 18:25:01 +0200

    no message

--- src/Blauhaus.Time/TimeService.cs
+++ src/Blauhaus.Time/TimeService.cs
@@ -11,18 +11,6 @@ namespace Blauhaus.Time
         public DateTimeOffset CurrentUtcOffset => DateTimeOffset.UtcNow;
         
         public DateTime CurrentLocalTime => DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Local);
-        
-        //public string GetRelativeTimeString(DateTime utcDateTime, CultureInfo culture = null)
-        //{
-        //    if(utcDateTime.Kind != DateTimeKind.Utc)
-        //        throw new ArgumentException("DateTime must be UTC");
-
-        //    return utcDateTime.Humanize(true, DateTime.UtcNow, culture);
-        //}
-
-        //public string GetTimeSpanString(TimeSpan timeSpan, CultureInfo culture, int precision = 1)
-        //{
-        //    return timeSpan.Humanize(precision, false, culture);
-        //}
+         
     }
 }
